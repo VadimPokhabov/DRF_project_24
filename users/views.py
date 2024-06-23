fc16c47b0f3e6688d2d87e404133b755c2cf0078
Vadim <VadimPokhabov@mail.ru>
2024-06-23 19:33:57 +0700

    add view for Payments

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payments
+from users.serializers import UserSerializer, PaymentsSerializer
 
 
 class UserViewSet(ModelViewSet):
@@ -10,3 +13,14 @@ class UserViewSet(ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PaymentsListAPIView(ListAPIView):
+    """
+    API для получения списка уроков
+    """
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lesson', 'payment_method')
+    ordering_fields = ('payment_date',)
