8f78311e07b8336fa72658aa192cb4c80a7af801
Vadim <VadimPokhabov@mail.ru>
2024-06-30 16:43:51 +0700

    Add urls 'token'

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserViewSet, PaymentsListAPIView
@@ -10,4 +12,6 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 urlpatterns = [
     path('payments/', PaymentsListAPIView.as_view(), name='payments_list'),
+    path('token/', TokenObtainPairView.as_view(permission_classes=[AllowAny]), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=[AllowAny]), name='token_refresh'),
 ] + router.urls
