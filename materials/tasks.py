1ed211f935a25ebf8c1735679d5f84d876ef7926
Vadim <VadimPokhabov@mail.ru>
2024-07-16 20:21:30 +0700

    Fix tasks

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,6 +1,5 @@
 from celery import shared_task
 from django.core.mail import send_mail
-
 from config import settings
 from materials.models import Subscription
 
@@ -8,7 +7,7 @@ from materials.models import Subscription
 @shared_task
 def send_email(course):
     """
-    Отправка письма об выходе урока
+    Отправка письма о выходе урока
     """
     subscriptions = Subscription.objects.filter(course=course)
     if subscriptions:
--- users/tasks.py
+++ users/tasks.py
@@ -1,19 +1,21 @@
+from datetime import *
+
 from celery import shared_task
 from dateutil.relativedelta import relativedelta
 from django.utils import timezone
+import pytz
 
+from config import settings
 from users.models import User
 
 
 @shared_task
 def check_last_login():
-    users = User.objects.all()
-    date_now = timezone.now()
+    """
+    Блокирует пользователя, если он не входил в систему более 30 дней
+    """
+    users = User.objects.filter(is_active=True)
     for user in users:
-        if user.last_login:
-            if user.last_login < (date_now - relativedelta(months=1)):
-                user.is_active = False
-                user.save()
-            else:
-                user.last_login = date_now
-                user.save()
+        if datetime.now(pytz.timezone(settings.TIME_ZONE)) - user.last_login > timedelta(days=30):
+            user.is_active = False
+            user.save()
