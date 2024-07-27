a9664598ffc37c1012744d3f29ca194ea5d27f55
Vadim <VadimPokhabov@mail.ru>
2024-07-27 21:10:09 +0700

    Add docstring

--- users/permissions.py
+++ users/permissions.py
@@ -2,11 +2,17 @@ from rest_framework import permissions
 
 
 class IsModerator(permissions.BasePermission):
+    """
+    Пермишен для модераторов
+    """
     def has_permission(self, request, view):
         return request.user.groups.filter(name="Moderator").exists()
 
 
 class IsOwner(permissions.BasePermission):
+    """
+    Пермишен для владельца лекций и курсов
+    """
     def has_object_permission(self, request, view, obj):
         if obj.owner == request.user:
             return True
