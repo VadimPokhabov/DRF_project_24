8dbf23cdea273a544cbe324f09434b6a6b4b63fb
Vadim <VadimPokhabov@mail.ru>
2024-07-27 22:46:08 +0700

    Fix settings.py

--- config/settings.py
+++ config/settings.py
@@ -64,12 +64,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT")
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
